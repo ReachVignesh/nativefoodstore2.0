f4bb6137f36ade55b347bb0c5b5078a42b422259
ReachVignesh <rajendrenvignesh@gmail.com>
2017-08-04 11:50:23 +0530

    Updated theme with added functionalities as per current requirement. Ignore all *._* files from the repo.

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/admin/');
+define('DIR_SYSTEM', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/');
+define('DIR_LANGUAGE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/admin/language/');
+define('DIR_TEMPLATE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/admin/view/template/');
+define('DIR_CONFIG', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/config/');
+define('DIR_IMAGE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/image/');
+define('DIR_CACHE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/download/');
+define('DIR_UPLOAD', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/upload/');
+define('DIR_LOGS', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/logs/');
+define('DIR_MODIFICATION', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/modification/');
+define('DIR_CATALOG', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'vktpl15');
+define('DB_DATABASE', 'nativef2_sg5671');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/catalog/');
+define('DIR_SYSTEM', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/');
+define('DIR_LANGUAGE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/config/');
+define('DIR_IMAGE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/image/');
+define('DIR_CACHE', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/download/');
+define('DIR_UPLOAD', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/upload/');
+define('DIR_MODIFICATION', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/modification/');
+define('DIR_LOGS', 'C:\Bitnami\wampstack-5.6.30-3\apache2\htdocs\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'vktpl15');
+define('DB_DATABASE', 'nativef2_sg5671');
+define('DB_PREFIX', 'oc_');
